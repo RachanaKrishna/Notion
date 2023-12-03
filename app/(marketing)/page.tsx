0bcc9578f94c8b0169d6cdfd480bd79d33839cfa
RachanaKrishna <rachana.krishna@cloudtaru.com>
2023-12-03 23:18:25 +0530

    commit

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,19 @@
+import { Footer } from "./_components/footer";
+import { Heroes } from "./_components/heroes";
+import { Heading } from "/Users/admin/notion/app/(marketing)/_components/heading"
+
+const MarketingPage = () => {
+  return (
+    <div className='min-h-full flex flex-col'>
+      <div className='flex flex-col items-center justify-center
+      md:justify-start text-center gap-y-8 flex-1 px-6 pb-10'>
+        <Heading />
+        <Heroes /> 
+
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default MarketingPage;
